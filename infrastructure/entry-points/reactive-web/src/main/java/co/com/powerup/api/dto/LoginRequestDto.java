968c36895d2d74f44cf9edd42fd4a8b81ee35ca0
Santiago <santiagovalenciarios@gmail.com>
2025-09-08 18:20:05 -0500

    feature(HU03): Se agregan anotaciones faltantes a los DTOs

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/api/dto/LoginRequestDto.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/api/dto/LoginRequestDto.java
@@ -1,6 +1,16 @@
 package co.com.powerup.api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequestDto {
+
+    @NotBlank
+    @Email
     private String username; //email
+
+    @NotBlank
     private String password;
 }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/api/dto/LoginResponseDto.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/api/dto/LoginResponseDto.java
@@ -1,5 +1,10 @@
 package co.com.powerup.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class LoginResponseDto {
 
     private String token;
