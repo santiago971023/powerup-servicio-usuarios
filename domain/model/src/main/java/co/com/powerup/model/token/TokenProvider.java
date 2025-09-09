ee66e5faeaf7f6e02951608c0437d20382b0c421
Santiago <santiagovalenciarios@gmail.com>
2025-09-08 20:13:19 -0500

    feature(HU03): Se agregan métodos 'validateToken' y 'getAllClaimsFromToken' al puerto 'TokenProvider.java'. El método 'getAllClaimsFromToken' retorna un objeto de tipo 'TokenClaims' que es mi representación de Dominio de Claims de jwt, que por arquitectura no podía usar a nivel de de dominio.

--- domain/model/src/main/java/co/com/powerup/model/token/TokenProvider.java
+++ domain/model/src/main/java/co/com/powerup/model/token/TokenProvider.java
@@ -1,10 +1,13 @@
 package co.com.powerup.model.token;
 
 import co.com.powerup.model.user.User;
-import reactor.core.publisher.Mono;
 
 public interface TokenProvider {
 
     String generateToken(User user);
+
+    boolean validateToken(String token);
+
+    TokenClaims getAllClaimsFromToken(String token);
 
 }
