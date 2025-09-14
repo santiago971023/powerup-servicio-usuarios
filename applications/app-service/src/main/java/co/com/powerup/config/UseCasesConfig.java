ec3bd22d82306976f084c4573751e766f4b3544a
Santiago <santiagovalenciarios@gmail.com>
2025-09-14 12:07:22 -0500

    feature(HU03): Se agrega creación manual de Beans de los Casos de uso en UseCasesConfig.java

--- applications/app-service/src/main/java/co/com/powerup/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/powerup/config/UseCasesConfig.java
@@ -1,10 +1,17 @@
 package co.com.powerup.config;
 
 
+import co.com.powerup.model.role.gateways.RoleRepository;
+import co.com.powerup.model.token.TokenProvider;
+import co.com.powerup.model.user.gateways.PasswordEncoderServicePort;
+import co.com.powerup.model.user.gateways.UserRepository;
+import co.com.powerup.usecase.LoginUseCase;
+import co.com.powerup.usecase.UserUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ComponentScan(basePackages = "co.com.powerup.usecase",
@@ -14,5 +21,16 @@ import org.springframework.context.annotation.FilterType;
         useDefaultFilters = false)
 public class UseCasesConfig {
 
+        @Bean
+        public LoginUseCase loginUseCase(UserRepository userRepository,
+                                         PasswordEncoderServicePort passwordEncoder,
+                                         TokenProvider tokenProvider) {
+                return new LoginUseCase(userRepository, passwordEncoder, tokenProvider);
+        }
+
+        @Bean
+        public UserUseCase userUseCase(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoderServicePort passwordEncoder) {
+                return new UserUseCase(userRepository, roleRepository, passwordEncoder);
+        }
 
 }
