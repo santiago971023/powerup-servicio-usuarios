2320201d31779d6fe8f14125f40d6a82ece2d39b
Santiago <santiagovalenciarios@gmail.com>
2025-09-16 16:12:49 -0500

    feature(HU04): Se agrega método en la interface del mapper para decirle a mapstruct como actuar con el role

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/api/mapper/IUserDtoMapper.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/api/mapper/IUserDtoMapper.java
@@ -2,8 +2,10 @@ package co.com.powerup.api.mapper;
 
 import co.com.powerup.api.dto.UserRequestDto;
 import co.com.powerup.api.dto.UserResponseDto;
+import co.com.powerup.model.role.Role;
 import co.com.powerup.model.user.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
@@ -11,7 +13,16 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface IUserDtoMapper {
 
+
+
     User toDomain(UserRequestDto requestDto);
     UserResponseDto toResponseDto(User user);
+
+    default Role map(String roleName) {
+        if (roleName == null) return null;
+        Role role = new Role();
+        role.setName(roleName);
+        return role;
+    }
 
 }
